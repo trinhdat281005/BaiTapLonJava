7618a7318fbcd2b0daca2e869250c11032982917
dat <trinhdat0971797927@gmail.com>
2025-11-04 10:31:07 +0700

    Initial commit

---
+++ BaitapLonJava_bkap/APP/AppMain.java
@@ -0,0 +1,10 @@
+package BaitapLonJava_bkap.APP;
+
+import BaitapLonJava_bkap.VIEW.MainView;
+
+public class AppMain {
+    public static void main(String[] args) {
+        MainView mv = new MainView();
+        mv.showMenu();
+    }
+}
